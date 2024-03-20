070e0535e3a06e4d360e57d5ed1e72a042f01e27
massifr <massifr@fastwebnet.it>
2024-03-20 19:05:05 +0100

    feat: first working export to ICML

--- src/icml_with_index.lua
+++ src/icml_with_index.lua
@@ -2,17 +2,25 @@
 
 local INDEX_REF_BEFORE = "before"
 local INDEX_TERM_CLASS = "index-term"
+-- a string used in ICML index topics
+local ICML_TOPICN = "Topicn"
 
 ---@diagnostic disable-next-line: undefined-global
 local pandoc = pandoc
+local string_find = string.find
+local string_gsub = string.gsub
+local string_sub = string.sub
 local table_insert = table.insert
 local table_concat = table.concat
 
-local function logging_info(...)
+local function logging_info(w)
+  io.stderr:write('(I) icml_with_index: ' .. w .. '\n')
 end
-local function logging_warning(...)
+local function logging_warning(w)
+  io.stderr:write('(W) icml_with_index: ' .. w .. '\n')
 end
-local function logging_error(...)
+local function logging_error(e)
+  io.stderr:write('(W) icml_with_index: ' .. e .. '\n')
 end
 local logging
 if pcall(require, "logging") then
@@ -32,17 +40,56 @@ local indicesFun = require('./pandoc-indices')
 
 local _isIndexRef = indicesFun.isIndexRef
 local hasClass = indicesFun.hasClass
+local INDEX_NAME_DEFAULT = indicesFun.INDEX_NAME_DEFAULT
 
 local function isIndexRef(span)
   return _isIndexRef(indices, span)
 end
 
-local get_reference_for_format = {
-  icml = function(idref, index_prefix)
-    if not idref then return end
-    local self_attr = ''                                                        -- ' Self="u301"'
-    local ref_topic_attr = ' ReferencedTopic="' .. index_prefix .. idref .. '"' -- ' ReferencedTopic="u115Topicnesempio"'
-    local id_attr = ''                                                          -- ' Id="1"'
+-- term_id_to_array_index has a table for every index
+-- that associates the id of an index term
+-- to the array index (offset) in terms[index_name]
+---@type table<string, table<string,integer>>
+local term_id_to_array_index = {}
+---Retrieves an index term that has an id.
+---@param index_name IndexName The name of the index.
+---@param id         string    The index term identifier.
+---@return IndexTerm|nil
+local function getIndexTermById(index_name, id)
+  local index_terms = terms[index_name]
+  if not term_id_to_array_index[index_name] then
+    local id_to_array_index = {}
+    for t = 1, #index_terms do
+      local term = index_terms[t]
+      if term.id then
+        id_to_array_index[term.id] = t
+      end
+    end
+    term_id_to_array_index[index_name] = id_to_array_index
+  end
+  local i = term_id_to_array_index[index_name][id]
+  if i then
+    return index_terms[i]
+  end
+end
+
+---Produce a reference to an index to be put in an ICML document.
+---@param idref        string The identifier of the index term.
+---@param index_name   string The name of the index the term belongs to.
+---@param index_prefix string The prefix of the index.
+local function getIcmlReference(idref, index_name, index_prefix)
+  if not idref then
+    logging_warning("can't get a reference without an idref")
+    return
+  end
+  local term = getIndexTermById(index_name or INDEX_NAME_DEFAULT, idref)
+  if term then
+    local self_attr = '' -- ' Self="u301"'
+    -- local ref_topic_attr = ' ReferencedTopic="' .. index_prefix .. idref .. '"' -- ' ReferencedTopic="u115Topicnesempio"'
+    local ref_topic_attr = ' ReferencedTopic="' ..
+        index_prefix .. ICML_TOPICN .. term.text ..
+        '"'            -- ' ReferencedTopic="u115Topicnesempio"'
+    local id_attr = '' -- ' Id="1"'
     local text = '<CharacterStyleRange AppliedCharacterStyle="CharacterStyle/$ID/[No character style]">\n'
         .. '  <PageReference'
         .. self_attr
@@ -53,6 +100,23 @@ local get_reference_for_format = {
         .. '</CharacterStyleRange>\n'
     return pandoc.List({ pandoc.RawInline('icml', text) })
   end
+  return pandoc.List({})
+end
+
+local function getIcmlTopic(prefix, term)
+  local text = (term.text or term.id or "") .. ""
+  -- text = string_gsub(text, "[\r\n ]+$", "")
+  -- text = string_gsub(text, '"', "&quot;")
+  -- logging_info('topic "' .. term.id .. '": ' .. text)
+  return '<Topic Self="'
+      .. prefix .. ICML_TOPICN .. term.sortKey
+      .. '" SortOrder="' .. (term.sortKey or '')
+      .. '" Name="' .. term.text
+      .. '" />'
+end
+
+local get_reference_for_format = {
+  icml = getIcmlReference,
 }
 
 -- This filter inserts the index references in the text.
@@ -68,7 +132,7 @@ local insert_index_references = {
         if get_reference then
           local inlines = pandoc.List({})
           ---@diagnostic disable-next-line: need-check-nil
-          local ref = get_reference(idref, index.prefix)
+          local ref = get_reference(idref, index.name, index.prefix)
           if ref then
             ---@diagnostic disable-next-line: need-check-nil
             if index.refWhere == INDEX_REF_BEFORE then
@@ -89,14 +153,6 @@ local insert_index_references = {
   end
 }
 
-local function writeTopic(prefix, name, sort_key)
-  return '<Topic Self="'
-      .. prefix .. name
-      .. '" SortOrder="' .. sort_key
-      .. '" Name="' .. name
-      .. '" />'
-end
-
 local place_indices = {
   Pandoc = function(doc)
     -- local indices_blocks = pandoc.List({})
@@ -114,7 +170,7 @@ local place_indices = {
       for t = 1, #index_terms do
         local term = index_terms[t]
         -- logging_info(term)
-        table_insert(index_lines, writeTopic(index.prefix, term.id, term.sortKey))
+        table_insert(index_lines, getIcmlTopic(index.prefix, term))
       end
       table_insert(index_lines, '</Index>')
     end
@@ -151,10 +207,10 @@ end
 
 function Template()
   local t = pandoc.template.default 'icml'
-  local story_start = string.find(t, '  <Story Self="pandoc_story"')
+  local story_start = string_find(t, '  <Story Self="pandoc_story"')
   if story_start then
     -- logging_info('STORY_START=' .. story_start)
-    t = string.sub(t, 1, story_start - 1) .. '$index$\n  ' .. string.sub(t, story_start)
+    t = string_sub(t, 1, story_start - 1) .. '$index$\n  ' .. string_sub(t, story_start)
   end
   return t
 end
--- src/pandoc-indices.lua
+++ src/pandoc-indices.lua
@@ -1,5 +1,4 @@
 --- A Pandoc custom writer to export an index in ICML.
-
 local INDEX_CLASS = "index"
 local INDEX_NAME_ATTR = "index-name"
 local INDEX_NAME_DEFAULT = "index"
@@ -12,19 +11,31 @@ local INDEX_REF_WHERE_DEFAULT = INDEX_REF_AFTER
 local INDEX_TERM_CLASS = "index-term"
 local INDEX_SORT_KEY_ATTR = "sort-key"
 
+local string_find = string.find
+local string_gsub = string.gsub
+local string_len = string.len
+local string_sub = string.sub
+local table_concat = table.concat
+local table_insert = table.insert
+local utf8len = pandoc.text.len
+local utf8lower = pandoc.text.lower
+local utf8sub = pandoc.text.sub
+
 ---@diagnostic disable-next-line: undefined-global
 local PANDOC_STATE = PANDOC_STATE
 ---@diagnostic disable-next-line: undefined-global
 local pandoc = pandoc
-local table_insert = table.insert
 local pandoc_Pandoc = pandoc.Pandoc
 local pandoc_write = pandoc.write
 
-local function logging_info(...)
+local function logging_info(w)
+  io.stderr:write('(I) pandoc-indices: ' .. w .. '\n')
 end
-local function logging_warning(...)
+local function logging_warning(w)
+  io.stderr:write('(W) pandoc-indices: ' .. w .. '\n')
 end
-local function logging_error(...)
+local function logging_error(e)
+  io.stderr:write('(W) pandoc-indices: ' .. e .. '\n')
 end
 local logging
 if pcall(require, "logging") then
@@ -36,13 +47,51 @@ if logging then
   logging_error = logging.error
 end
 
+---@class List A Pandoc List.
+
+---@class Block A Pandoc Block.
+---@field content List The content of the Block.
+
+---@class Inline A Pandoc Inline.
+---@field content List The content of the Inline.
+
+---@class Div: Block A Pandoc Div.
+---@field identifier string
+---@field classes string[]
+---@field attributes {[string]: string}
+---@field content Block[]
+
+---@class Span: Inline A Pandoc Span.
+---@field identifier string
+---@field classes string[]
+---@field attributes {[string]: string}
+---@field content Inline[]
+
+---@alias IndexName string The name of an index.
+
+---@class Index    An index in a document.
+---@field name     string The name of the index, e.g. "index", "names", etc.
+---@field refClass string The .
+---@field refWhere INDEX_REF_BEFORE|INDEX_REF_AFTER Where the reference is put.
+---@field prefix   string A prefix of the index.
+
+---@class IndexTerm A term inside an Index.
+---@field id      string  The term identifier.
+---@field sortKey string  The key to sort the term with.
+---@field text    string  The content of the term as a string without styles.
+---@field blocks  Block[] The content of the term as Pandoc Blocks.
+
+---@type Index[] An array of indices defined in a document.
 local indices = {}
+---@type string The current index during parsing.
 local current_index_name
+---@type table<IndexName,IndexTerm[]> An associative array index name => index terms.
 local terms = {}
 
---- Check whether a Pandoc item with an Attr has a class.
---@param elem the Block or Inline with an Attr
---@param class the class to look for among the ones in Attr's classes
+---Check whether a Pandoc item with an Attr has a class.
+---@param elem Block|Inline The Block or Inline with an Attr.
+---@param class string      The class to look for among the ones in Attr's classes.
+---@return boolean
 local function hasClass(elem, class)
   if elem and elem.attr and elem.attr.classes then
     local classes = elem.attr.classes
@@ -55,6 +104,10 @@ local function hasClass(elem, class)
   return false
 end
 
+---Search for an Index that satisfies a predicate.
+---@param indexes   Index[] An array of indices.
+---@param predicate fun(index: Index): boolean
+---@return Index|nil
 local function findIndexWith(indexes, predicate)
   for i = 1, #indexes do
     local index = indexes[i]
@@ -65,10 +118,19 @@ local function findIndexWith(indexes, predicate)
   return nil
 end
 
+---Search for an Index with a specific name.
+---@param name IndexName The name of the index you are looking for.
+---@return Index|nil
 local function findIndexWithName(name)
   return findIndexWith(indices, function(i) return i.name == name end)
 end
 
+---Verify if a Span is an index reference.
+---If it's an index reference returns true and the Index.
+---@param indexes Index[] The known index of a document.
+---@param span    Span  A Pandoc Span.
+---@return boolean
+---@return Index|nil
 local function isIndexRef(indexes, span)
   local index = findIndexWith(
     indexes,
@@ -82,19 +144,21 @@ local function isIndexRef(indexes, span)
   return false
 end
 
---- Checks whether a `Div` is meant to define an index.
+---Checks whether a `Div` is meant to define an index.
+---@param div Div A Pandoc Div.
+---@return boolean
 local function isIndexDiv(div)
   return hasClass(div, INDEX_CLASS)
 end
 
---- Get the index corresponding to the `Div`, if it's an index `Div`.
---@param div the `Div` block that could be an index `Div`
---@param log more info when it's `true`
---@returns the index or `nil`
+---Get an index object corresponding to the `Div`, if it's an index `Div`.
+---@param div Div   The `Div` block that could be an index `Div`.
+---@param log boolean Produce a log message as side effect.
+---@return Index|nil
 local function indexFromDiv(div, log)
   if isIndexDiv(div) then
     if log then
-      logging_info('Div has the "' .. INDEX_CLASS .. '" class, so I guess it\' an index definition')
+      logging_info('Div has the "' .. INDEX_CLASS .. '" class, so I guess it\'s an index definition')
     end
     local attrs = div.attributes
     local name = attrs[INDEX_NAME_ATTR] or INDEX_NAME_DEFAULT
@@ -119,6 +183,12 @@ local function indexFromDiv(div, log)
   return nil
 end
 
+---Get an index term object, if it's an index term `Div`.
+---@param div Div A Pandoc `Div`.
+---@return IndexName|nil # the name of the index.
+---@return string|nil    # the identifier of the term.
+---@return string|nil    # the sort key of the term.
+---@return Block[]|nil   # the content `Block`s of the `Div`.
 local function indexTermFromDiv(div)
   if hasClass(div, INDEX_TERM_CLASS) then
     local attrs = div.attributes
@@ -129,6 +199,120 @@ local function indexTermFromDiv(div)
   end
 end
 
+-- CODE TO COMPUTE SORT KEYS
+local MIN_SORT_KEY_LENGTH = 10 -- min length of a computed sort key
+local MAX_SORT_KEY_LENGTH = 40 -- max length of a computed sort key
+local NON_LETTER_CHAR = "_"    -- all non letter chars are converted to this one
+
+-- a synthetic table to describe the translation of accented characters
+-- included: latin base, supplement, extended A, B, C, extended+, IPA, phonetic exts, extra phonetic exts
+-- not included: cyrillic, combining chars, subscript and superscript forms
+local ACCENTED_TRANSLATION = {
+  { to = "a", oneof = "àáâãäåāăąǎǟǡǻȁȃȧḁẚạảấầẩẫậắằẳẵặⱥ" },
+  { to = "b", oneof = "ƀƃɓḃḅḇ" },
+  { to = "c", oneof = "çćĉċčƈȼɕḉ" },
+  { to = "d", oneof = "ďđƌȡɖɗᵭᶁḋḍḏḑḓ" },
+  { to = "e", oneof = "èéêëēĕėęěȅȇȩɇᶒḕḗḙḛḝẹẻẽếềểễệⱸ" },
+  { to = "f", oneof = "ƒᵮᶂḟ" },
+  { to = "g", oneof = "ĝğġģǥǧǵɠᶃḡ" },
+  { to = "h", oneof = "ĥħȟɦḣḥḧḩḫẖ" },
+  { to = "i", oneof = "ìíîïĩīĭįıǐȉȋɨᵢᶖḭḯỉị" },
+  { to = "j", oneof = "ĵǰɉʝ" },
+  { to = "k", oneof = "ķĸƙǩᶄḱḳḵⱪ" },
+  { to = "l", oneof = "ĺļľŀłƚȴɫɬɭᶅḷḹḻḽⱡ" },
+  { to = "m", oneof = "ɱᵯᶆḿṁṃ" },
+  { to = "n", oneof = "ñńņňŉƞǹȵɲɳᵰᶇṅṇṉṋ" },
+  { to = "o", oneof = "òóôõöøōŏőơǒǫǭǿȍȏȫȭȯȱṍṏṑṓọỏốồổỗộớờởỡợⱺ" },
+  { to = "q", oneof = "ɋʠ" },
+  { to = "r", oneof = "ŕŗřȑȓɍɼɽɾᵣᵲᵳᶉṙṛṝṟ" },
+  { to = "s", oneof = "śŝşšſșȿʂᵴᶊṡṣṥṧṩ" },
+  { to = "t", oneof = "ţťŧƫƭțȶʈᵵṫṭṯṱẗⱦ" },
+  { to = "u", oneof = "ùúûüũūŭůűųưǔǖǘǚǜȕȗʉᵤᶙṳṵṷṹṻụủứừửữự" },
+  { to = "v", oneof = "ʋᵥᶌṽṿⱱⱴ" },
+  { to = "w", oneof = "ŵẁẃẅẇẉẘⱳ" },
+  { to = "x", oneof = "ᶍẋẍ" },
+  { to = "y", oneof = "ýÿŷƴȳɏẏẙỳỵỷỹỿ" },
+  { to = "z", oneof = "źżžƶȥɀʐʑᵶᶎẑẓẕⱬ" },
+  { to = "ae", oneof = "æǣǽ" },
+  { to = "dz", oneof = "ǆǳ" },
+  { to = "ij", oneof = "ĳ" },
+  { to = "lj", oneof = "ǉ" },
+  { to = "ll", oneof = "ỻ" },
+  { to = "nj", oneof = "ǌ" },
+  { to = "oe", oneof = "œ" },
+  { to = "ss", oneof = "ß" },
+}
+
+local UTF8_TO_UNACCENTED = nil -- a table for accented => not accented translation
+local function computeUtf8ToUnaccented()
+  UTF8_TO_UNACCENTED = {}
+  for i = 1, #ACCENTED_TRANSLATION do
+    local acc = ACCENTED_TRANSLATION[i]
+    local to, oneof = acc.to, acc.oneof
+    local utfchar
+    for j = 1, utf8len(oneof) do
+      utfchar = utf8sub(oneof, j, j)
+      UTF8_TO_UNACCENTED[utfchar] = to
+    end
+  end
+end
+
+-- lowercase a UTF8 string and remove accents
+local function lowerAndRemoveAccents(text)
+  if not UTF8_TO_UNACCENTED then
+    computeUtf8ToUnaccented()
+  end
+  local unaccented = {}
+  local utfchar, unacc
+  local locase = utf8lower(text)
+  for i = 1, utf8len(locase) do
+    utfchar = utf8sub(locase, i, i)
+    if string_len(utfchar) == 1 then
+      table_insert(unaccented, utfchar)
+    else
+      ---@diagnostic disable-next-line: need-check-nil
+      unacc = UTF8_TO_UNACCENTED[utfchar]
+      if unacc then
+        table_insert(unaccented, unacc)
+      else
+        table_insert(unaccented, NON_LETTER_CHAR)
+      end
+    end
+  end
+  return table_concat(unaccented)
+end
+
+-- computes a sort key from a UTF8 text
+-- the sort key will contain only unaccented latin letters, numbers and NON_LETTER_CHAR chars;
+-- NON_LETTER_CHAR can't be at the beginning or at the end of the sort key
+-- there can't be no consecutive NON_LETTER_CHARs
+local function computeSortKey(text)
+  local index = string_find(text, "%(")
+  if not index or index > MAX_SORT_KEY_LENGTH then
+    index = 0
+    local prev_index
+    while index and index < MIN_SORT_KEY_LENGTH do
+      prev_index = index
+      index = string_find(text, " ", index + 1)
+    end
+    if index and index > MAX_SORT_KEY_LENGTH then
+      if prev_index >= MIN_SORT_KEY_LENGTH and prev_index <= MAX_SORT_KEY_LENGTH then
+        index = prev_index
+      else
+        index = nil
+      end
+    end
+    index = index or MAX_SORT_KEY_LENGTH
+  end
+  local sort_key = string_sub(text, 1, index - 1)
+  -- sort_key = string_gsub(sort_key, " +$", "")
+  sort_key = lowerAndRemoveAccents(sort_key)
+  sort_key = string_gsub(sort_key, "[^0-9a-z]+", NON_LETTER_CHAR)
+  sort_key = string_gsub(sort_key, "^" .. NON_LETTER_CHAR .. "+", "")
+  sort_key = string_gsub(sort_key, NON_LETTER_CHAR .. "+$", "")
+  return sort_key
+end
+
 local function addIndexTerm(index_name, id, sort_key, content)
   local index_terms = terms[index_name]
   if not index_terms then
@@ -136,10 +320,18 @@ local function addIndexTerm(index_name, id, sort_key, content)
     index_terms = terms[index_name]
   end
   local content_as_doc = pandoc_Pandoc(content)
+  local sortKey = sort_key
+  if not sortKey then
+    sortKey = computeSortKey(pandoc_write(pandoc_Pandoc(content), 'plain', { wrap_text = "preserve" }))
+    -- logging_info('Text for sort key: "' .. sortKey .. '"')
+  end
+  local text = pandoc_write(content_as_doc, "plain", { wrap_text = "preserve" })
+  text = string_gsub(text, "[\r\n ]+$", "")
+  text = string_gsub(text, '"', "&quot;")
   table_insert(index_terms, {
     id = id,
-    sortKey = sort_key,
-    text = pandoc_write(content_as_doc, "plain"),
+    sortKey = sortKey,
+    text = utf8sub(text, 1, 40), -- TODO: the text must be trimmed only in ICML, not here
     blocks = content_as_doc.blocks
   })
 end
@@ -175,7 +367,7 @@ local collect_indices = {
     else
       local index_name, id, sort_key = indexTermFromDiv(div)
       if index_name then
-        addIndexTerm(index_name, id, sort_key)
+        addIndexTerm(index_name, id, sort_key, div.content)
       end
     end
   end
@@ -197,4 +389,5 @@ return {
   isIndexRef = isIndexRef,
   findIndexWith = findIndexWith,
   hasClass = hasClass,
+  INDEX_NAME_DEFAULT = INDEX_NAME_DEFAULT,
 }
