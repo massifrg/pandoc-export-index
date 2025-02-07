fc7fb8790fc8ebc736c0602e79c461895c5367e3
massifrg <massifrg@gmail.com>
2025-02-07 13:19:41 +0100

    placeholder for script that adds indices in the metadata of a doc

---
+++ src/put_terms_in_metadata.lua
@@ -0,0 +1,9 @@
+--[[
+    put_terms_in_metadata.lua: A [Pandoc filter](https://pandoc.org/lua-filters.html)
+                               to put index terms in the metadata of a document.
+    Copyright: (c) 2025 M. Farinella
+    License:   MIT - see LICENSE file for details
+    Usage:     See README.md for details
+]]
+
+-- TODO
