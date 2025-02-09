f918c23872720198121d82a0f88a411399247a00
massifrg <massifrg@gmail.com>
2025-02-09 23:13:13 +0100

    wip

--- src/put_terms_in_metadata.lua
+++ src/put_terms_in_metadata.lua
@@ -3,7 +3,10 @@
                                to put index terms in the metadata of a document.
     Copyright: (c) 2025 M. Farinella
     License:   MIT - see LICENSE file for details
-    Usage:     See README.md for details
-]]
+    Usage:     pandoc -V indices='{ "indices": [ ... ], "terms": { ... } }' -L put_terms_in_metadata.lua ...
+               pandoc -V indices-file=indices.json -L put_terms_in_metadata.lua ...
+--]]
 
--- TODO
+--[[
+
+--]]
